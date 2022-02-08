64c328ee2721f0317750c7ff7783b16f0976d495
Suzanne <STian@lcs.on.ca>
2022-02-08 09:14:28 -0500

    basic static version of the interface

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("X")
+                    .font(.largeTitle)
+                Spacer()
+            
+                VStack {
+                    Text("5")
+                    Text("6")
+                }
+                .font(.largeTitle)
+            }
+            Divider()
+        }
+        .padding(50)
+       
     }
 }
 
