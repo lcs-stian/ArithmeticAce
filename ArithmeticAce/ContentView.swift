389028b032166f00125053c07f61e0f88051b873
Suzanne <STian@lcs.on.ca>
2022-02-11 14:23:11 -0500

    finish, the start of day two

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -2,75 +2,89 @@
 //  ContentView.swift
 //  ArithmeticAce
 //
-//  Created by Suzanne Tian on 2022-02-08.
+//  Created by Russell Gordon on 2022-02-07.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     
-    
-    let mulpiplicand = Int.random(in: 1...12)
+    // MARK: Stored properties
+    let multiplicand = Int.random(in: 1...12)
     let multiplier = Int.random(in: 1...12)
-    
-    //holds the user's input
     @State var inputGiven = ""
     
-    var correctAnswer: Int {
-        return mulpiplicand * multiplier
-        
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctProduct: Int {
+        return multiplicand * multiplier
     }
+    
     var body: some View {
-        VStack (spacing:0) {
-            HStack {
                 
-                Text("X")
+        VStack(spacing: 0) {
+            HStack {
+                Text("✕")
                 
                 Spacer()
                 
                 VStack(alignment: .trailing) {
-                    Text("\(mulpiplicand)")
+                    Text("\(multiplicand)")
                     Text("\(multiplier)")
                 }
-                
             }
             
             Divider()
             
             HStack {
-                
-                
+                Image(systemName: "checkmark.circle")
+                    .foregroundColor(.green)
+                    //        CONDITION      true  false
+                    .opacity(answerCorrect ? 1.0 : 0.0)
                 Spacer()
-                
                 TextField("",
                           text: $inputGiven)
                     .multilineTextAlignment(.trailing)
             }
             
             Button(action: {
-                //Guard statment to connect the input
-                guard let answerGiven = Int(inputGiven) else {
-                    //inputis invalid
+                
+                // Answer has been checked!
+                answerChecked = true
+                
+                // Convert the input given to an integer, if possible
+                guard let productGiven = Int(inputGiven) else {
+                    // Sadness, not a number
+                    answerCorrect = false
                     return
                 }
-                //check the answer
-                if answerGiven == correctAnswer {
-                    //show the checkmarks
+
+                // Check the answer!
+                if productGiven == correctProduct {
+                    // Celebrate! 👍🏼
+                    answerCorrect = true
+                } else {
+                    // Sadness, they gave a number, but it's correct 😭
+                    answerCorrect = false
                 }
-                
             }, label: {
-                //Label
-                Text("Check answer")
+                Text("Check Answer")
                     .font(.largeTitle)
             })
-                .buttonStyle(.bordered)
                 .padding()
+                .buttonStyle(.bordered)
             
+            Spacer()
         }
-        .padding()
+        .padding(.horizontal)
         .font(.system(size: 72))
-        
-        
+
         
     }
 }
